79dff84ddc09866c0c68eec3872c0f263a5871c5
KJHan1256 <149484123+KJHan1256@users.noreply.github.com>
2024-04-05 21:07:34 +0900

    플레이어 추가
    임시 스프라이트로 플레이어 추가
    충돌과 이동을 위해 플레이어 오브젝트에 rigidbody와 callrider추가
    플레이어 이동 구현 (방향키 or wasd)

---
+++ GP_teamProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 inputVec;
+    Rigidbody2D rigid;
+
+    [SerializeField] private float speed = 2.0f;      //이동속도 설정, 스크립트 외부에서도 조절 가능하게 [SerializeField] 선언
+
+
+    //Awake는 오브젝트 생성시 동작, 따라서 가장 처음 동작되는 부분
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();    //player오브젝트의 rigidbodt2D를 캐싱
+        Application.targetFrameRate = 100;      //프레임을 100으로 고정
+    }
+
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");     //키보드 방향키와 ad로 좌우 입력받음
+        inputVec.y = Input.GetAxisRaw("Vertical");         //키보드 방향키와 ws로 상하...
+        
+        Vector2 movement = inputVec.normalized * speed * Time.deltaTime;
+        rigid.MovePosition(rigid.position + movement);
+        
+    }
+
+}

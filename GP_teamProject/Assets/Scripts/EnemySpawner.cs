de4a0aebd7f0a18a364d94d9364f7751b4d8ab3c
KJHan1256 <149484123+KJHan1256@users.noreply.github.com>
2024-05-31 14:52:31 +0900

    적 티어 및 강화 추가
    적 티어와 그에 따른 적과 배경의 변화 개발 완료
    점수에 따라 적이 강해지는 시스템을 추가로 개발 완료

--- GP_teamProject/Assets/Scripts/EnemySpawner.cs
+++ GP_teamProject/Assets/Scripts/EnemySpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
@@ -7,12 +8,69 @@ public class EnemySpawner : MonoBehaviour
 
     [SerializeField] private StageData stageData;       //적 생성 위치를 잡기 위한 스테이지 데이터
     [SerializeField] private float spawnTime = 2f;      //적 생성 주기, 기본 2초
-    [SerializeField] private GameObject enemyPrefab;    //생성할 적 프리팹
+    [SerializeField] private int enemyTier;  //적의 티어, 이에 따라 생성할 적 변경
+    [SerializeField] private List<GameObject> enemyPrefabList;    //생성할 적 프리팹 리스트
+
+    private GameObject enemySpawn;     //생성할 적
+
+    [SerializeField] private GameObject background; //배경 변경을 위한 게임 오브젝트를 받을 공간
+
+    private BackgroundScroller bg;  //배경 변경을 위한 스크립트를 받을 공간
+
+    [Header("tier change score")]
+    [SerializeField] private int tier2Score;    //이 점수를 넘으면 2티어로 상승
+    [SerializeField] private int tier3Score;    //이 점수를 넘으면 3티어로 상승
+
+    
+
 
     private void Awake()
     {
+        //적 티어 및 생성할 적 초기화
+        enemyTier = 1;
+        enemySpawn = enemyPrefabList[enemyTier - 1];   
+
+        //배경 변경을 위한 스크립트 받기
+        bg = background.GetComponent<BackgroundScroller>();
+
+        //적 생성 시작
         StartCoroutine("SpawnEnemy");
     }
+
+    private void Update()
+    {
+        if(PlayerStatus.instance.playerTier != 3)
+        {
+            int sc = PlayerStatus.instance.score;
+            if (enemyTier == 1 && sc >= tier2Score) 
+            {
+                ChangeEnemyTier(2);
+            }
+            else if(enemyTier == 2 && sc >= tier3Score)
+            {
+                ChangeEnemyTier(3);
+            }
+        }
+    }
+
+
+    //적 티어 변경 함수
+    public void ChangeEnemyTier(int targetTier)
+    {
+        if(targetTier == 2 || targetTier == 3)
+        {
+            enemyTier = targetTier;
+            enemySpawn = enemyPrefabList[enemyTier - 1];
+            bg.ChangeBackground(enemyTier);
+            print("Enemy tier changed: " + enemyTier);
+        }
+        else
+        {
+            print("wrong tier input");
+            return;
+        }
+    }
+
 
     private IEnumerator SpawnEnemy()
     {
@@ -20,7 +78,7 @@ public class EnemySpawner : MonoBehaviour
         {
             float spawnPositionY = Random.Range(stageData.LimitMin.y, stageData.LimitMax.y);
             //생성할 y값 지정
-            Instantiate(enemyPrefab, new Vector3(stageData.LimitMax.x + 1.0f, spawnPositionY, 0.0f), Quaternion.identity);
+            Instantiate(enemySpawn, new Vector3(stageData.LimitMax.x + 1.0f, spawnPositionY, 0.0f), Quaternion.identity);
             //적 생성
             yield return new WaitForSeconds(spawnTime);
             //spawnTime만큼 대기
